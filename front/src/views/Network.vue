72c96e72e4737ab1711cbe063ec32ceb0816f0a0
yaskevich <alyaxey.yaskevich@gmail.com>
2021-03-23 23:33:08 +0300

    render view with a graph [front]

---
+++ front/src/views/Network.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="chart-holder">
+    <Graph :data="data" />
+  </div>
+</template>
+
+<script>
+import Graph from "../components/Graph.vue";
+export default {
+  name: "Dashboard",
+  components: {
+    Graph,
+  },
+  data() {
+    return {
+      data: [10, 40, 15, 25, 50],
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style>
+.chart-holder {
+  text-align: center;
+  color: #2c3e50;
+  max-width: 720px;
+  margin: 100px auto;
+  padding: 0 20px;
+}
+svg {
+  /* important for responsiveness */
+  display: block;
+  fill: none;
+  stroke: none;
+  width: 100%;
+  height: 100%;
+  overflow: visible;
+  background: #eee;
+}
+
+</style>
