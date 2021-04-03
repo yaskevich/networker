dc8434cceffae870a96b666700dabbd03450fbaf
yaskevich <alyaxey.yaskevich@gmail.com>
2021-04-03 23:30:57 +0300

    enable PrimeVue styles for app [front]

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 createApp(App)
   .use(PrimeVue)
